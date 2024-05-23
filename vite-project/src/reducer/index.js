79574460c9f2811bba1fcc81e43df157109d6fe4
airaawat <miraj.doshi@bosctechlabs.com>
2024-05-23 16:42:59 +0530

    developed simple dashboard pages and also pages related to add course

---
+++ vite-project/src/reducer/index.js
@@ -0,0 +1,17 @@
+import { combineReducers } from "@reduxjs/toolkit";
+
+import authReducer from "../slices/AuthSlice";
+import profileReducer from "../slices/ProfileSlice";
+import cartReducer from "../slices/CartSlice";
+import courseReducer from "../slices/CourseSlice";
+import viewCourseReducer from "../slices/ViewCourseSlice";
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+  profile: profileReducer,
+  cart: cartReducer,
+  course: courseReducer,
+  viewCourse: viewCourseReducer,
+});
+
+export default rootReducer;
